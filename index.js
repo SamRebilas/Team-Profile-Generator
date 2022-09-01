b6209a45629a1acf7668c539c61ffb73d7c181be
Samrebilas <sam.rebilas@gmail.com>
2022-09-01 12:49:35 -0400

    index.js returns array without the add another employee function. working on solution

---
+++ index.js
@@ -0,0 +1,58 @@
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
+const generatePage = require("./dist/generate-html");
+const inquirer = require("inquirer");
+const fs = require("fs")
+
+
+function employeeQuestions(Employees) {
+  return inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What is your role in the company?",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+      { 
+        type: "input", 
+      name: "name", 
+      message: "What is the employees name?" },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employees id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employees email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "what is the employees office number? If you do not have an office number type: N/A",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school did you go to? type: N/A if you are not an intern",
+      },
+      {
+        type: "confirm",
+        name: "Add",
+        message: "Would you like to add another employee?",
+        default: false,
+
+
+      }
+    ])
+    .then((newEmployee) => {
+        const {Add} = newEmployee
+      return Add ? employeeQuestions(Employees) : Employees
+});
+}
+employeeQuestions()
+.then((employeeInfo) => employeeQuestions(employeeInfo))
+ .then((Array)=> console.log(Array))
